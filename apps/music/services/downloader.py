b7831faf04e1e1c7e4c1b189f71287e5ba36fc08
AlfonsoDiaz09 <alfonsodiazc.09@gmail.com>
2026-01-24 23:15:37 -0600

    feat: Download layout and funcion download with .zip

---
+++ apps/music/services/downloader.py
@@ -0,0 +1,20 @@
+import os
+import yt_dlp
+
+def download_mp3(url: str, base_path: str):
+    os.makedirs(base_path, exist_ok=True)
+
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": os.path.join(base_path, "%(uploader)s/%(title)s.%(ext)s"),
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
+        }],
+        "noplaylist": True,
+        "quiet": True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.extract_info(url, download=True)
